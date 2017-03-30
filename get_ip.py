ece6ddc78098b01dc36db1b5ce02fbc75075dd34
LiuWenJu <lwj037303@gmail.com>
2017-03-30 15:13:45 +0800

    获得本机ip信息和杀死指定pid的脚本

---
+++ get_ip.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+"""
+author:liuwenju
+time:2017-3-30
+email:liuwenju0123@gmail.com
+description:get the ip info of the host  
+"""
+
+import requests
+import os,commands
+
+
+cmd = "ifconfig  | grep  'inet addr:' | grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}'"
+ip = commands.getoutput(cmd)
+url = "http://freeapi.ipip.net/" + ip
+
+req = requests.get(url)
+print req.text
+
---
+++ kill-pid.py
@@ -0,0 +1,29 @@
+#coding=utf-8
+
+"""
+author: liuwenju
+time:2017-3-16
+email:liuwenju0123@gmail.com
+descriptiom: kill the pid which you want 
+"""
+import os
+import commands
+
+
+#('./pipesocks pump -p 7473 -k qwer1234 > /dev/null')
+
+def getPid(process):
+    cmd = 'ps -aux | grep "%s"'% process
+    infos = commands.getoutput(cmd)
+    info = infos.split('\n')
+    selfid = info[-1].split()[1]
+    pid = info[1].split()[1]
+    if pid == selfid:
+        print 'the same pid,do nothing!'
+    else:
+        commands.getoutput('kill -9 "%s"'% pid)
+
+if __name__ == '__main__':
+    process = 'pipesocks'#infomation of pid you want kill
+    getPid(process)
+
